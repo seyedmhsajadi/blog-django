353e678fcdca26be36a428d51474cda93ddfe867
seyed hadi <s.hadi.sajadi.sn@gmail.com>
2026-02-13 02:40:15 -0800

    switching database to postgresql & adding some feature in index view

--- blog/models.py
+++ blog/models.py
@@ -70,8 +70,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', verbose_name="پست")
     name = models.CharField(max_length=250, verbose_name="نام")
     body = models.TextField(verbose_name="متن کامنت")
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = jmodels.jDateTimeField(auto_now_add=True)
+    updated = jmodels.jDateTimeField(auto_now=True)
     active = models.BooleanField(default=False)
 
     class Meta:
